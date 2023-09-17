90db73afd57267675f126b597ff2e2bb119fe350
Priyanshu79 <119873582+7Priyanshu9@users.noreply.github.com>
2023-09-18 01:04:01 +0530

    pdf to image converter added

--- app.py
+++ app.py
@@ -1,69 +1,12 @@
-import pytesseract
-from PIL import Image
-from googletrans import Translator
+from pdf2image import convert_from_path
 
-myconfig = r"--psm 6 --oem 3"
+# Update the Poppler path with escaped backslashes
+poppler_path = r'C:\Program Files\poppler-23.08.0\Library\bin'
 
-# Open the image and perform text extraction
-image_path = "tiger.jpg"
-img = Image.open(image_path)
-text = pytesseract.image_to_string(img, config=myconfig)
+# Provide the path to your PDF file
+pdf_file_path = 'sample.pdf'
 
-# Print the extracted text
-print("Extracted Text:")
-print(text)
+images = convert_from_path(pdf_file_path, 500, poppler_path=poppler_path)
 
-# Language selection menu
-print("\nSelect a language for translation:")
-print("1. Bengali (bn)")
-print("2. Hindi (hi)")
-print("3. Telugu (te)")
-print("4. Tamil (ta)")
-print("5. Urdu (ur)")
-print("6. Gujrati (gu)")
-print("7. Kannada (kn)")
-print("8. Malayalam (ml)")
-print("9. Marathi (mr)")
-print("10. Nepali (ne)")
-print("11. Odia (or)")
-print("12. Punjabi (pa)")
-print("13. Sindhi (sd)")
-
-choice = input("Enter your choice (1/2/3/4/5/6/7/8/9/10/11/12/13): ")
-
-# Translate the extracted text based on the user's choice
-translator = Translator()
-
-if choice == "1":
-    op = translator.translate(text, dest="bn")
-elif choice == "2":
-    op = translator.translate(text, dest="hi")
-elif choice == "3":
-    op = translator.translate(text, dest="te")
-elif choice == "4":
-    op = translator.translate(text, dest="ta")
-elif choice == "5":
-    op = translator.translate(text, dest="ur")
-elif choice == "6":
-    op = translator.translate(text, dest="gu")
-elif choice == "7":
-    op = translator.translate(text, dest="kn")
-elif choice == "8":
-    op = translator.translate(text, dest="ml")
-elif choice == "9":
-    op = translator.translate(text, dest="mr")
-elif choice == "10":
-    op = translator.translate(text, dest="ne")
-elif choice == "11":
-    op = translator.translate(text, dest="or")
-elif choice == "12":
-    op = translator.translate(text, dest="pa")
-elif choice == "13":
-    op = translator.translate(text, dest="sd")
-else:
-    print("Invalid choice. Using default target language (Bengali).")
-    op = translator.translate(text, dest="bn")
-
-# Print the translated text
-print("\nTranslated Text:")
-print(op.text)
+for i, image in enumerate(images):
+    image.save('page' + str(i) + '.jpg', 'JPEG')
